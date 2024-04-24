cdd83c850d10b3fca0313175bccf037da044f515
lunarinaa <aydan.akbarova@gmail.com>
2024-04-24 10:21:00 +0400

    Revert "fix"
    This reverts commit 4dd5cb1b477c8d80ded445e2d0cf4622932bf2f0.

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""   Export data in the JSON format.
-"""
+"""exporting data in json format"""
+
+
 import json
 import requests
 
 
-def main():
-    """According to user_id, export information in json
-    """
-    users_uri = 'https://jsonplaceholder.typicode.com/users'
-    users = requests.get(users_uri).json()
-    info = {}
+def export_data():
+    """Saving all the data to JSON file"""
+    users_route = 'https://jsonplaceholder.typicode.com/users'
+    todos_route = 'https://jsonplaceholder.typicode.com/todos/?userID={}'
+    users = requests.get(users_route).json()
+    data = {}
 
     for user in users:
         user_id = user.get('id')
-        name = user.get('username')
-        todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-            user_id)
-        request_todo = requests.get(todos).json()
+        username = user.get('username')
+        todos = todos_route.format(user_id)
+        todo_request = requests.get(todos).json()
         tasks = []
-        for todo in request_todo:
-            task = {"username": name, "task": todo.get("title"),
-                    "completed": todo.get("completed")}
+        for todo in todo_request:
+            task = {"username": username,
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed')}
             tasks.append(task)
-        info[user_id] = tasks
+        data[user_id] = tasks
 
     with open('todo_all_employees.json', 'w+') as file:
-        file.write(json.dumps(info))
+        file.write(json.dumps(data))
 
 
 if __name__ == "__main__":
-    main()
+    export_data()
